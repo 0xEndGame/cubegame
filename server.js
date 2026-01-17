4db7937144b0588ea6389c2bd2315a4105d367ff
cryptoUser <defifarmerhenry@gmail.com>
2026-01-17 22:26:58 +0100

    sol

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,solana-client');
     if (req.method === 'OPTIONS') {
         return res.sendStatus(204);
     }
